dc026fa8828bc6d654665a3333da5849883742c1
= <=>
2018-12-16 22:13:47 +0530

    Basic layout 1

--- controller/indexController.js
+++ controller/indexController.js
@@ -1,7 +1,15 @@
 module.exports = function(app){
   const bodyParser = require('body-parser');
-
   app.get('/' , function(req,res){
-    res.render('home');
+    res.render('home', {activeLink : 0});
+  });
+  app.get('/album', function(req,res){
+    res.render('album', {activeLink : 1});
+  })
+  app.get('/songs', function(req,res){
+    res.render('songs', {activeLink : 2});
+  });
+  app.get('/albumDetails/:album', function(req,res){
+    res.render('albumDetails' , {album : req.params.album,activeLink : 1});
   });
 }
